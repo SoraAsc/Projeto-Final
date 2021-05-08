6fe641d5040846e5e0c8a117f0b6141349e885d9
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2021-05-08 00:26:06 -0300

    Inserção de botões para as informações básicas.

--- SolarSystemApp/src/data/Database.js
+++ SolarSystemApp/src/data/Database.js
@@ -8,6 +8,27 @@ export default  [
       id: '001',
       name: 'Mercúrio',
       imageLink: require('./../../assets/images/mercurio.jpg'),
+      informacoesBasicas: {
+        ordemOrbital: 1,
+        distanciaDoSol: '0,46 UA',
+        massa: '3,3011x(10)^23 kg',
+        volume: ' 6,083x(10)^10 km³',
+        densidade: '5,427 g/cm³',
+        areaDaSuperficie: '7,48x(10)^7 km²',
+        temperatura: '426°C a -183°C  ',
+        periodoDeRotacao: '58,646 dias terrestres',
+        periodoDeRevolucao: '115,88 dias terrestres',
+        gravidade: '3,7 m/s²',
+        velocidadeDeEscape: '4,25 km/s',
+        satelites: 0,
+      },
+      curiosidades: [
+        {
+          id: '001',
+          titulo: 'Temperatura,  tamanho e atmosfera.',
+          historia: "Mercúrio passou a ser o menor planeta do Sistema Solar em Agosto de 2006 devido ao rebaixamento de Plutão, além de menor, Mercúrio também é o mais inferior planeta do Sistema Solar, entretanto apresenta a segunda maior temperatura do mesmo ficando abaixo de, por incrível que pareça, Vênus, segundo planeta inferior. Ainda, falando de suas condições térmicas, Mercúrio possui a maior variação de temperatura de todo o Sistema Solar chegando entre os 600°C, isso acontece devido a falta de atmosfera do planeta, consequência de sua baixa gravidade. É importante levantar o seguinte ponto sobre a atmosfera de Mercúrio, apesar de se dizer que o planeta Mercúrio não apresenta atmosfera, entre 1960 e 1970 a sonda planetária Mariner 10, uma das únicas duas sondas enviadas para Mercúrio, conseguiu encontrar a presença de uma camada de hélio sobre o planeta, o'que gerou grande discussão no mundo científico levando a conclusão de que Mercúrio possui, sim, uma atmosfera, porém quase inexistente."
+        },
+      ]
     },
     {
       id: '002',
--- SolarSystemApp/src/pages/Details.js
+++ SolarSystemApp/src/pages/Details.js
@@ -1,34 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   StyleSheet,
   Text,
   Image,
   View,
   ScrollView,
+  FlatList,
 } from 'react-native';
 
-import { useNavigation, useNavigationState } from '@react-navigation/native'
+import DetailsShowButton from './../../components/DetailsShowButton';
 
 export default function Details(props){
-    console.log(props.route.params.data.imageLink)
+    //console.log(props.route.params.data.imageLink)
+    const data = props.route.params.data
+    //console.log(data.curiosidades[0])
     return (
         <View style={styles.container}>
           <View style={{flex: 0.5}}>
-            <Image style={styles.itemimage} source={props.route.params.data.imageLink}>                
+            <Image style={styles.itemimage} source={data.imageLink}>                
             </Image>
           </View>
+          
           <View style={{flex:0.5}}>
+            
             <ScrollView>
-            <Text style={{color:'white'}}>
-              Ex aliqua fugiat labore mollit minim fugiat laboris sunt id magna tempor anim dolore consectetur. Reprehenderit pariatur eu exercitation deserunt non ut ea. Culpa cupidatat aliqua duis deserunt et et irure reprehenderit qui velit magna adipisicing occaecat pariatur.
+              <View style={{backgroundColor:'white',}}>
+                <Text style={styles.title}>Informações Básicas</Text>            
+              </View>
 
-              Duis esse officia velit sunt sit cillum irure qui Lorem. Esse consectetur pariatur labore ea occaecat proident ex ipsum esse consequat commodo ad proident. Ex qui nulla nulla sunt aliqua. Consequat aliquip nisi non amet dolore ut. Exercitation officia cillum in eu veniam laborum eiusmod ipsum. Dolore anim labore aute culpa.
+              <View style={{flex:1, flexDirection:'column', justifyContent:'space-between', flexWrap: 'wrap'}}>
+                <DetailsShowButton name="Ordem Orbital" value={data.informacoesBasicas.ordemOrbital}/>
+                <DetailsShowButton name="Distância Do Sol" value={data.informacoesBasicas.distanciaDoSol}/>
+                <DetailsShowButton name="Massa" value={data.informacoesBasicas.massa}/>
+                <DetailsShowButton name="Volume" value={data.informacoesBasicas.volume}/>
+                <DetailsShowButton name="Densidade" value={data.informacoesBasicas.densidade}/>
+                <DetailsShowButton name="Área da Superfície" value={data.informacoesBasicas.areaDaSuperficie}/>
+                <DetailsShowButton name="Temperatura" value={data.informacoesBasicas.temperatura}/>
+                <DetailsShowButton name="Período de Rotação" value={data.informacoesBasicas.periodoDeRotacao}/>
+                <DetailsShowButton name="Período De Revolução" value={data.informacoesBasicas.periodoDeRevolucao}/>
+                <DetailsShowButton name="Gravidade" value={data.informacoesBasicas.gravidade}/>
+                <DetailsShowButton name="Velocidade De Escape" value={data.informacoesBasicas.velocidadeDeEscape}/>
+                <DetailsShowButton name="Satélites" value={data.informacoesBasicas.satelites}/>
+              </View>
 
-              Magna consequat esse enim laboris labore proident consequat labore sit incididunt ea et. Adipisicing sunt qui sunt ullamco velit. Eu ea reprehenderit laborum nulla elit aliquip culpa do eu culpa irure. Laboris consectetur nostrud et sunt. Veniam non officia Lorem est tempor.
-              Ex aliqua fugiat labore mollit minim fugiat laboris sunt id magna tempor anim dolore consectetur. Reprehenderit pariatur eu exercitation deserunt non ut ea. Culpa cupidatat aliqua duis deserunt et et irure reprehenderit qui velit magna adipisicing occaecat pariatur.
+              <View style={{backgroundColor:'white',}}>
+                <Text style={styles.title}>Curiosidades</Text>            
+              </View>
 
-              </Text>
+              <View>
+                <FlatList 
+                  data={data.curiosidades}
+                  keyExtractor={ item=> item.id}
+                  scrollEnabled={false}
+                  showsVerticalScrollIndicator={false}
+                  //renderItem={TopiItem}
+                  renderItem = {({item,index}) =>(
+                    <View>
+                      <Text style={{color: 'white'}}>{index+1}. {item.titulo}</Text>
+                      <Text style={{color: 'white'}}>{item.historia}</Text>
+                    </View>
+                  )}
+                >
+                </FlatList>
+              </View>
             </ScrollView>
+
           </View>
         </View>
     )
@@ -50,5 +86,10 @@ const styles = StyleSheet.create({
       borderTopLeftRadius: 0,
       borderTopRightRadius: 0,
     },
+    title: {
+      color:'black', 
+      fontSize:25,
+      textAlign:'center',
+    }
 
   });
